44e7cdea787c19646f3c88ba8cbd0ed5da02b9cd
Yufan You <ouuansteve@gmail.com>
2020-05-27 14:01:51 +0800

    Remove copyright notice about CP Editor (#1)

--- include/LSPClient.hpp
+++ include/LSPClient.hpp
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2019-2020 Ashar Khan <ashar786khan@gmail.com>
- *
- * This file is part of cpeditor.
- *
- * cpeditor is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * I will not be responsible if cpeditor behaves in unexpected way and
- * causes your ratings to go down and or lose any important contest.
- *
- * Believe Software is "Software" and it isn't immune to bugs.
- *
- */
-
 #ifndef LSPCLIENT_HPP
 #define LSPCLIENT_HPP
 
